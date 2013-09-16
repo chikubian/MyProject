ca978757fb388a5823bdc70924f683c7a6e92511
chikubian <chikubian@gmail.com>
2013-09-16 23:10:44 +1000

    Moved button

--- MyProject/Form1.Designer.cs
+++ MyProject/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(145, 90);
+            this.button2.Location = new System.Drawing.Point(160, 177);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
